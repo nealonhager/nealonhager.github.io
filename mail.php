af4414f113e970153e6d7ef5f3786a6b84db0daa
nealon hager <nealonhager@gmail.com>
2017-12-24 12:55:40 -0700

    hopefully fix contact sheet

--- mail.php
+++ mail.php
@@ -5,6 +5,6 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 $content = "From: \n$name \n $email \n\n Message: \n $message";
 $recipient = "nealonhager@gmail.com";
-mail($recipient,$subject,$message,"website contact sheet") or die("Error!");
+mail($recipient,$subject,$message) or die("Error!");
 echo "Thanks!";
 ?>
