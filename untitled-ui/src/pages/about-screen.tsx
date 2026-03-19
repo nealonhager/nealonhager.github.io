06699f3d306a45b0be682ab3ee28c7a767b956ff
Nealon Hager <nealonhager@gmail.com>
2026-03-18 21:01:20 -0700

    sticky header

--- untitled-ui/src/pages/about-screen.tsx
+++ untitled-ui/src/pages/about-screen.tsx
@@ -16,7 +16,7 @@ export const AboutScreen = () => {
             <div className="mt-16 flex w-full flex-col items-center gap-6 px-4 mx-auto lg:w-1/2">
                 <h1 className="py-16 text-center text-display-lg font-semibold text-primary">About Me</h1>
                 <Image src="/images/me.jpeg" alt="Me rock climbing at smith rock" className="mx-auto" />
-                <p className="text-lg w-full">
+                <p className="text-lg w-full text-primary">
                     I'm a software engineer with a passion for building web applications that are fast, responsive, and easy to use.
                     I have a strong background in web development, and laboratory automation. 
                     I've been having a lot of fun orchestrating agentic workflows recently.
